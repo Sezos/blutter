bb9d5989158d56c8cc7bdd263fca8b3e46ee9785
badral.e <badrist@icloud.com>
2025-04-20 17:58:47 +0800

    added example

---
+++ example/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:blutter/blutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const BText('My Flutter Package Example')),
+        body: const Center(
+          child: BText(
+            "Do i really need to do this? \t - No\n am i gonna do it anyways? - Absolutely",
+          ),
+        ),
+      ),
+    );
+  }
+}
